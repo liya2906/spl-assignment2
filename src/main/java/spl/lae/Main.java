e389379920274a419ca481093e391fb87fad3bab
faigler <faigler@post.bgu.ac.il>
2026-01-01 17:12:01 +0200

    fixed main()

--- src/main/java/spl/lae/Main.java
+++ src/main/java/spl/lae/Main.java
@@ -1,23 +1,33 @@
 package spl.lae;
 
 import java.io.IOException;
+import java.text.ParseException;
+
 import parser.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        LinearAlgebraEngine LAE = new LinearAlgebraEngine(10);
-        InputParser inputParser = new InputParser();
-
-        try {
-            ComputationNode root = inputParser.parse("example.json");
-            ComputationNode result = LAE.run(root);
-            OutputWriter.write(result.getMatrix(), "My_out.json");
-
-        } catch (Exception e) {
-            OutputWriter.write("Error: " + e.getMessage(), "My_out.json");
-        } finally {
-            System.out.println(LAE.getWorkerReport());
+        if (args.length < 3) {
+            System.err.println("Error: Missing arguments.");
+            return;
         }
+        int numThreads = Integer.parseInt(args[0]);
+        String inputPath = args[1];
+        String outputPath = args[2];
+        LinearAlgebraEngine lae = new LinearAlgebraEngine(numThreads);
+        InputParser inputParser = new InputParser();
+        try {
+            ComputationNode root = inputParser.parse(inputPath);
+            ComputationNode res = lae.run(root);
+            OutputWriter.write(res.getMatrix(), outputPath);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } catch (RuntimeException e) {
+            OutputWriter.write("Error: " + e.getMessage(), outputPath);
+        } finally {
+            System.out.println(lae.getWorkerReport());
+        }
+
     }
 }
